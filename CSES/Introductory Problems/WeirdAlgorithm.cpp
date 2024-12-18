a022830c8df1b9eb907fe91715755fe26153611e
krrattos <mayank.meerut.india@gmail.com>
2024-12-18 13:44:19 +0530

    First problem

---
+++ CSES/Introductory Problems/WeirdAlgorithm.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Macros for common operations
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+int main()
+{
+    ll n;
+    cin>>n;
+    while(n!=1)
+    {
+        cout<<n<<" ";
+        if(n%2==0)  // odd
+            n=n/2;
+        else
+            n=n*3+1;
+    }
+    cout<<"1";
+    return 0;
+}
